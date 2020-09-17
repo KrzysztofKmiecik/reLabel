5d1033e8cfe1a85285d0f813f728a9980a0b7be2
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2020-09-17 15:32:00 +0200

    oppacity

--- src/main/java/pl/javastart/hellofx/Main.java
+++ src/main/java/pl/javastart/hellofx/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
@@ -14,7 +15,14 @@ public class Main extends Application {
         BorderPane mainPane = FXMLLoader.load(getClass().getResource("/MainView.fxml"));
         Scene scene = new Scene(mainPane);
         stage.setScene(scene);
-        stage.setTitle("hello fx");
+
+
+        //  stage.setFullScreen(true);
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setOpacity(0.5);
+
+        stage.setTitle("reLabel");
         stage.show();
 
     }
